e677f22d2ba59d01828dfa9a104385e70cfec13b
Hongbo <12580159+ya0guang@users.noreply.github.com>
2024-03-28 17:15:03 -0400

    fix openkwargs in bees.py

--- beeswithmachineguns/bees.py
+++ beeswithmachineguns/bees.py
@@ -607,7 +607,7 @@ def _create_request_time_cdf_csv(results, complete_bees_params, request_time_cdf
         # see http://python3porting.com/problems.html#csv-api-changes
         openmode = IS_PY2 and 'w' or 'wt'
         openkwargs = IS_PY2 and {} or {'encoding': 'utf-8', 'newline': ''}
-        with open(csv_filename, openmode, openkwargs) as stream:
+        with open(csv_filename, openmode, **openkwargs) as stream:
             writer = csv.writer(stream)
             header = ["% faster than", "all bees [ms]"]
             for p in complete_bees_params:
